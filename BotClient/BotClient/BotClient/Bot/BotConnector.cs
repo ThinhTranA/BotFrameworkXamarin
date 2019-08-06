33d512b381dfdecc4d03043423b1c4e279823aa5
Thinh Tran <thinhtran@outlook.com.au>
2019-08-06 22:33:02 +0800

    Add chat Layout

--- BotClient/BotClient/BotClient/Bot/BotConnector.cs
+++ BotClient/BotClient/BotClient/Bot/BotConnector.cs
@@ -12,43 +12,43 @@ namespace BotClient.Bot
 {
     public class BotConnector
     {
-        private HttpClient httpClient;
-        private Conversation lastConversation;
+        private HttpClient _httpClient;
+        private Conversation _lastConversation;
         private string _directLineKey = "get key from Azure portal bot";
 
         public async Task Setup()
         {
-            httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("https://directline.botframeworks.com/api/conversations");
-            httpClient.DefaultRequestHeaders.Accept.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("BotConnector", _directLineKey);
-            var response = await httpClient.GetAsync("/api/tokens/");
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://directline.botframeworks.com/api/conversations");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("BotConnector", _directLineKey);
+            var response = await _httpClient.GetAsync("/api/tokens/");
 
             if(response.IsSuccessStatusCode)
             {
                 var conversation = new Conversation();
                 HttpContent contentPost = new StringContent(JsonConvert.SerializeObject(conversation), Encoding.UTF8,
                     "application/json");
-                response = await httpClient.PostAsync("/api/conversations/", contentPost);
+                response = await _httpClient.PostAsync("/api/conversations/", contentPost);
                 if(response.IsSuccessStatusCode)
                 {
                     var conversationInfo = await response.Content.ReadAsStringAsync();
-                    lastConversation = JsonConvert.DeserializeObject<Conversation>(conversationInfo);
+                    _lastConversation = JsonConvert.DeserializeObject<Conversation>(conversationInfo);
                 }
             }
         }
 
         public async Task<BotMessage> SendMessage(string username, string messageText)
         {
-            var messageToSend = new BotMessage() { From = username, Text = messageText, ConversationId = lastConversation.ConversationId };
+            var messageToSend = new BotMessage() { From = username, Text = messageText, ConversationId = _lastConversation.ConversationId };
             var contentPost = new StringContent(JsonConvert.SerializeObject(messageToSend), Encoding.UTF8, "application/json");
-            var conversationUrl = "https://directline.botframeworks.com/api/conversations/" + lastConversation.ConversationId + "/messages/"
+            var conversationUrl = "https://directline.botframeworks.com/api/conversations/" + _lastConversation.ConversationId + "/messages/";
 
-            var response = await httpClient.PostAsync(conversationUrl, contentPost);
+            var response = await _httpClient.PostAsync(conversationUrl, contentPost);
             var messageInfo = await response.Content.ReadAsStringAsync();
 
-            var messageReceived = await httpClient.GetAsync(conversationUrl);
+            var messageReceived = await _httpClient.GetAsync(conversationUrl);
             var messageReceivedData = await messageReceived.Content.ReadAsStringAsync();
             var messageRoot = JsonConvert.DeserializeObject<BotMessageRoot>(messageReceivedData);
             var messages = messageRoot.Messages;
--- BotClient/BotClient/BotClient/MainPage.xaml.cs
+++ BotClient/BotClient/BotClient/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BotClient.Bot;
+using BotClient.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -13,9 +16,45 @@ namespace BotClient
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private BotConnector _botConnector;
+        public ObservableCollection<BotMessage> ConversationList { get; }
+        private bool _progressVisible;
+        public bool ProgressVisible
+        {
+            get => _progressVisible;
+            set
+            {
+                _progressVisible = value;
+                OnPropertyChanged("ProgressVisible");
+            }
+        }
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            ConversationList = new ObservableCollection<BotMessage>();
+            _botConnector = new BotConnector();
         }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await _botConnector.Setup();
+        }
+
+        private async void  SendButton_Clicked(object sender, EventArgs e)
+        {
+            await SendMessage();
+        }
+
+        private async Task SendMessage()
+        {
+            ProgressVisible = true;
+            var botResponse = await _botConnector.SendMessage("Daniel", Message.Text);
+            ProgressVisible = false;
+            ConversationList.Add(botResponse);
+        }
+
+      
     }
 }
